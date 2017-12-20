1b9a2d637aceb65bad3244f35d08b2aae3358f1e
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2017-12-19 19:05:22 -0800

    Crazy Cat Lady high low game

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -5,27 +5,45 @@ package elseif;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
+		int random = new Random().nextInt(100)+1;
 		// 2. Print out the random variable above
-		
+		System.out.println(random);
 		// 11. do the following 10 times
+		for (int i = 0; i < 10; i++) {
+			
 		
 			// 1. ask the user for a guess using a pop-up window, and save their response 
-
+		String guess=JOptionPane.showInputDialog("Guess the number from 1-100");
 			// 4. convert the users’ answer to an int (Integer.parseInt(string))
-			
+	int choice=Integer.parseInt(guess);
 			// 5. if the guess is correct
 				// 6. win
+		if(choice==random) {
+			JOptionPane.showMessageDialog(null,"You won!" );
+			System.exit(0);
+	}
 			// 7. if the guess is high
 				// 8. tell them it's too high
+		else if (choice> random) {
+			JOptionPane.showMessageDialog(null,"Too high");
+	}
 			// 9. if the guess is low
 				// 10. tell them it's too low
-
+		else{
+			JOptionPane.showMessageDialog(null,"Too low");
+		}
+	}
 		// 12. tell them they lose
+		JOptionPane.showMessageDialog(null, "You Lost! HA");
+			
+			
+			
 	}
 
 }
